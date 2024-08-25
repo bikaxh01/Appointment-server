6ad8e607355f715ee113bc6cad210791298deee4
Bikash Mishra <mishrabikash202@gmail.com>
2024-08-25 15:47:02 +0530

    update

--- dist/config/EnvConfigs.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BUCKET_NAME = exports.PORT = void 0;
-exports.PORT = process.env.PORT;
-exports.BUCKET_NAME = process.env.BUCKET_NAME;
--- dist/routes/admin.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminRoute = void 0;
-const express_1 = require("express");
-exports.adminRoute = (0, express_1.Router)();
-exports.adminRoute.get("/", (req, res) => {
-    res.json({ message: "Admin Route are working" });
-});
